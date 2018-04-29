9f520b36866f02ce9e474583134d9dd3e5c3ea7c
antoine.drabble <antoine.drabble@gmail.com>
2018-04-29 23:24:08 +0200

    Create board

--- main.c
+++ main.c
@@ -1,25 +1,46 @@
+/**
+ * Author: Antoine Drabble & Loic Stankovic
+ * Date: 29.04.2018
+ * Description: 8-Puzzle solver
+ *              The board contains the number of each cell. The cell 0 corresponds to the empty space.
+ */
+
 #include <omp.h>
 #include <stdio.h>
 #include <stdlib.h>
 
+#define BOARD_SIZE 3
+
+int* board;
+
+struct Node{
+    int*
+};
+
 int main(int argc, char *argv[]) {
-    int nthreads, tid;
-
-    /* Fork a team of threads giving them their own copies of variables */
-    #pragma omp parallel private(nthreads, tid)
-    {
-
-        /* Obtain thread number */
-        tid = omp_get_thread_num();
-        printf("Hello World from thread = %d\n", tid);
-
-        /* Only master thread does this */
-        if (tid == 0) {
-            nthreads = omp_get_num_threads();
-            printf("Number of threads = %d\n", nthreads);
+    // Create 2d array
+    /*int** board = (int**)malloc(BOARD_SIZE * sizeof(int*));
+    int i, j;
+    for(i = 0; i < BOARD_SIZE; i++){
+        board[i] = (int*)malloc(BOARD_SIZE * sizeof(int));
+        for(j = 0; j < BOARD_SIZE; j++) {
+            board[i][j] = i + j * BOARD_SIZE;
         }
+    }*/
+    board = (int*)malloc(BOARD_SIZE * BOARD_SIZE * sizeof(int));
+    int i;
+    for(i = 0; i < BOARD_SIZE * BOARD_SIZE; i++){
+        board[i] = i;
+    }
 
-    }  /* All threads join master thread and disband */
+    dfs();
 
     return EXIT_SUCCESS;
+}
+
+void dfs(){
+    #pragma omp parallel{
+        #pragma omp for shared(board)
+
+    }
 }
