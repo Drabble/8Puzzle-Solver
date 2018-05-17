115ee980d6c6b74ae662fee9d12d89770c3ec841
Loïc Stankovic <loicstankovic@hotmail.com>
2018-05-17 22:17:25 +0200

    Refactor the code to fix dfs

--- main.c
+++ main.c
@@ -1,131 +1,147 @@
-/**
- * Author: Antoine Drabble & Loic Stankovic
- * Date: 29.04.2018
- * Description: 8-Puzzle solver
- *              The board contains the number of each cell. The cell 0 corresponds to the empty space.
- */
-
-#include <omp.h>
-#include <time.h>
-#include <stdio.h>
 #include <stdlib.h>
-#include <stddef.h>
-#include <math.h>
+#include <stdio.h>
+#include <string.h>
+
 #include "hashmap.c"
 
-#define BOARD_SIZE 3
+#define BOARD_SIDE 3
+#define BOARD_LENGTH BOARD_SIDE * BOARD_SIDE
+#define MAX_DEPTH 50
+#define COLOR 0
 
-void swap(int *board, size_t from, size_t to) {
-    int tmp = board[to];
-    board[to] = board[from];
-    board[from] = tmp;
-}
-
-void shuffle(int *array, size_t n) {
-    srand(time(NULL));
-    if (n > 1) {
-        size_t i;
-        for (i = 1; i < n - 1; i++) {
-            size_t j = rand() % (i + 1);
-            if (j == 0)
-                j = 1;
-
-            swap(array, i, j);
-        }
-    }
-
-}
-
-void print_board(const int *board, size_t size) {
-    for (size_t i = 0; i < size; i++) {
-        for (size_t j = 0; j < size; j++) {
-            printf("%d ", board[j + i * size]);
+/**
+ * Print the square board
+ * 
+ * @param board the board array
+ * @param size  the size of the board's side
+ */
+void print_board(const int *board, int size) {
+    for (int i = 0; i < size; i++) {
+        for (int j = 0; j < size; j++) {
+            int b = board[j + i * size];
+            if (b == 0 && COLOR)
+                printf("\033[1;31m");
+            printf("%d ", b);
+            if (b == 0 && COLOR)
+                printf("\033[0m");
         }
         printf("\n");
     }
+    printf("---\n");
 }
 
-int dfs(struct hashmap *hm, const int board[], int depth) {
 
-    // Find 0
+/**
+ * Check if the board is a solution. The correct solution is:
+ *
+ *      1 2 3
+ *      4 5 6
+ *      7 8 0
+ * 
+ * @param  board the board array to check
+ * @return       1 if the solution is correct, 0 otherwise
+ */
+int check_solved(const int *board, int size) {
+    if (board[size - 1] != 0)
+        return 0;
+
+    for (int i = 0; i < size - 2; ++i)
+        if (board[i] != i + 1)
+            return 0;
+
+    return 1;
+}
+
+/**
+ * Create a copy of an array and swap two of its elements 
+ * @param origin pointer to the original array
+ * @param dest   pointer to the destination array
+ * @param size   size of the array
+ * @param from   first element to swap
+ * @param to     second element to swap
+ */
+void swap(const int *origin, int *dest, int size, int from, int to) {
+    memcpy(dest, origin, size * sizeof(int));    
+    int temp = dest[to];
+    dest[to] = dest[from];
+    dest[from] = temp;
+}
+
+
+/**
+ * Solve the 8-puzzle using the recursive depth-first traversal
+ * 
+ * @param  board the board array
+ * @param  depth the current depth of the recursion
+ * @return       a boolean indicating if a solution is found (1 for found, 0 otherwise)
+ */
+int solve_dfs(const int board[], struct hashmap *explored, int depth) {
+    if (check_solved(board, BOARD_LENGTH)){
+        printf("solved!\n");
+        return 1;
+    }
+    
+    if (depth > MAX_DEPTH)
+        return 0;
+
     int pos;
-    for (pos = 0; pos < BOARD_SIZE * BOARD_SIZE; pos++) if (board[pos] == 0) break;
+    for (pos = 0; pos < BOARD_LENGTH; pos++) if (board[pos] == 0) break;
 
-    // Get possible moves
-    int left = pos - BOARD_SIZE;
-    int right = pos + BOARD_SIZE;
-    int up = pos % BOARD_SIZE == 0 ? -1 : pos - 1;
-    int down = pos % BOARD_SIZE == BOARD_SIZE - 1 ? -1 : pos + 1;
-    int directions[] = {left, right, up, down};
+    // compute the different moves, -1 if not possible
+    int directions[] = {
+        pos % BOARD_SIDE == 0 ? -1 : pos - 1, 
+        pos % BOARD_SIDE == BOARD_SIDE - 1 ? -1 : pos + 1,
+        pos - BOARD_SIDE,
+        pos + BOARD_SIDE
+    };
 
-    int i;
-    for (i = 0; i < 4; i++) {
-        if (directions[i] >= 0 && directions[i] < BOARD_SIZE * BOARD_SIZE) {
-            int *newBoard = malloc(BOARD_SIZE * BOARD_SIZE * sizeof(int));
-            int j;
+    for (int i = 0; i < 4; i++) {
+        int direction = directions[i];
+        if (direction >= 0 && direction < BOARD_LENGTH) {
+            int* new_board = malloc(BOARD_LENGTH * sizeof(int));
 
-            // Update the board with the move
-            for (j = 0; j < BOARD_SIZE * BOARD_SIZE; j++) {
-                if (j == pos) {
-                    newBoard[j] = board[directions[i]];
-                } else if (j == directions[i]) {
-                    newBoard[j] = board[pos];
-                } else {
-                    newBoard[j] = board[j];
-                }
-            }
+            // swap the 0 with a possible index
+            swap(board, new_board, BOARD_LENGTH, pos, direction);
 
-            // Check if solved
-            int solved = 1;
-            for (j = 0; j < BOARD_SIZE * BOARD_SIZE; j++) {
-                if (j != newBoard[j]) {
-                    solved = 0;
-                    break;
-                }
-            }
-            if (solved == 1) {
-                printf("Solved\n");
-                printf("Path is (in reverse order): \n");
-                print_board(newBoard, BOARD_SIZE);
-                printf("\n");
-                return 1;
-            }
-
-            // TODO Fix the problem and return the path and limit the depth if(depth > 10)
-            // Dfs
-            if (hashmap_set(hm, newBoard) == 0) {
-                if(dfs(hm, newBoard, depth + 1)){
+            // if this configuration is not yet explored
+            if (hashmap_set(explored, new_board))
+            {
+                // if a possible solution is found
+                if (solve_dfs(new_board, explored, depth + 1))
+                {
+                    print_board(new_board, BOARD_SIDE);
+                    free(new_board);
                     return 1;
                 }
-            } else {
-                free(newBoard);
             }
+            free(new_board);
         }
     }
+
     return 0;
 }
 
-// Hashmap
-// Trouver fonction/heurisitique pour avoir un bon choix initial
-// Trouver une première solution rapidement de façon à éliminer les autres
-// On fait un dfs mais qui s'arrête quand il dépasse une longueur de parcours plus grande que la meilleur solution trouvée jusqu'à maintenant
-int main(int argc, char *argv[]) {
-    int board[] = {0, 1, 2, 3, 4, 5, 6, 7, 8};
-    //int board[] = {0,1,2,3,4,5,6,7,8};
-    //shuffle(board, (size_t) BOARD_SIZE * BOARD_SIZE);
 
-    print_board(board, (size_t) BOARD_SIZE);
+int main(int argc, char const *argv[])
+{
+    int board[] = { 1, 2, 0, 3, 4, 5, 6, 7, 8};
+    //int board[] = { 0, 5, 3, 4, 2, 7, 6, 8, 1};
+    //int board[] = { 5,6,7,4,0,8,3,2,1};
+    //int board[] = { 1,3,4,8,6,2,7,0,5};
+    //int board[] = {1,2,3,8,0,4,7,6,5};
 
-    struct hashmap *hm;
-    hm = hashmap_create(64, BOARD_SIZE * BOARD_SIZE); // TODO: Trouver une bonne taille pour la hashmap
+    struct hashmap *explored;
+    explored = hashmap_create(100000000, BOARD_LENGTH); // TODO: Trouver une bonne taille pour la hashmap
 
-    if (dfs(hm, board, 0) == 1) {
-        printf("Found the shortest path");
+
+    if (solve_dfs(board, explored, 0))
+    {
+        printf("solution found\n");
     } else {
-        printf("Impossible level");
+        printf("no solution found\n");
     }
 
-    hashmap_free(hm);
+    hashmap_free(explored);
 
-    return EXIT_SUCCESS;
+    return 0;
 }
